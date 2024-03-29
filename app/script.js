96b8a7d0ded2f19316eddd35c4dd027cceecced5
JanaJambresic <jambresic.jana@gmail.com>
2024-03-29 20:15:12 +0100

    Added code for checking matching pairs.

--- app/script.js
+++ app/script.js
@@ -1,18 +1,24 @@
 const desc = document.querySelector(".desc");
 let btnStart = desc.querySelector(".start");
 let gameBoard = document.querySelector(".cards-container");
+let firstCard, secondCard;
 let cards = [];
+let matchedPairs = 0;
 
-fetch('./cards/cards_data.json')
-    .then((res) => res.json())
-    .then((json) => {
-        cards = [...json, ...json];
-    });
+async function getCards() {
+    const response = await fetch('./cards/cards_data.json');
+    const jsonData = await response.json();
+    cards = [...jsonData, ...jsonData];
+}
 
-btnStart.addEventListener('click', function () {
-    btnStart.style.display = "none";
-    createCardsAndShuffle();
-});
+function hideCards() {
+    setTimeout(() => {
+        cards.forEach((c) => {
+            let cardImg = document.querySelector(`img[src="./${c.url}"]`);
+            cardImg.src = "./cards/back.png";
+        });
+    }, 1000);
+}
 
 function createCardsAndShuffle() {
     for (let i = cards.length - 1; i > 0; i--) {
@@ -21,14 +27,63 @@ function createCardsAndShuffle() {
     }
     for (let c of cards) {
         let card = document.createElement("div");
-        card.setAttribute("data-id", c.id);
         card.classList.add("card");
+        card.setAttribute("data-name", c.name);
+        card.setAttribute("data-id", c.id);
         card.innerHTML = `
-        <div class="image">
-            <img src="./${c.url}" alt="${c.name}">
-        </div>
-        `;
+            <div class="image">
+                <img src="./${c.url}" alt="${c.name}">
+            </div>
+            `;
         gameBoard.appendChild(card);
+        card.addEventListener('click', flipCard);
     }
 }
 
+function flipCard() {
+    if (secondCard || this.classList.contains("flip")) return;
+    this.classList.add("flip");
+    let cardId = this.getAttribute("data-id");
+    let getCard = cards.find((c) => c.id == cardId);
+    let cardImg = this.querySelector("img");
+    cardImg.src = `./${getCard.url}`;
+    if (!firstCard) {
+        firstCard = this;
+    } else {
+        secondCard = this;
+        checkMatch();
+    }
+}
+
+function checkMatch() {
+    if (firstCard && secondCard) {
+        if (firstCard.getAttribute("data-id") === secondCard.getAttribute("data-id")) {
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+            resetBoard();
+            matchedPairs++;
+        } else {
+            setTimeout(() => {
+                firstCard.querySelector("img").src = "./cards/back.png";
+                secondCard.querySelector("img").src = "./cards/back.png";
+                firstCard.classList.remove("flip");
+                secondCard.classList.remove("flip");
+                resetBoard();
+            }, 1000);
+        }
+    }
+}
+
+function resetBoard() {
+    firstCard = null;
+    secondCard = null;
+}
+
+
+btnStart.addEventListener('click', function () {
+    btnStart.style.display = "none";
+    getCards().then(() => {
+        createCardsAndShuffle();
+        hideCards();
+    });
+});
